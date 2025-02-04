59874dc4b3ebd7b301aa11aa57816b954beebe73
ShivaniMane <shivanimane43@gmail.com>
2025-02-04 22:44:23 +0530

    Initial commit with project set up

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "./components/ui/button"
+
+function App() {
+
+  return (
+    <div className="flex gap-2 items-center justify-center">
+      <h1 className="text-center text-lg">Ticket-Hub</h1>
+      <Button>Click</Button>
+    </div>
+  )
+}
+
+export default App
