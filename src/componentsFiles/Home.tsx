aa40e0ff07e53aa18bfed6fa6edbd866d52ed654
ShivaniMane <shivanimane43@gmail.com>
2025-02-05 18:02:33 +0530

    feat: implement the modal to save add ticket

--- src/componentsFiles/Home.tsx
+++ src/componentsFiles/Home.tsx
@@ -1,10 +1,15 @@
 import { SidebarTrigger } from "@/components/ui/sidebar";
-
+import BookTicket from "./BookTicket";
 const Home = () => {
+
   return (
     <>
       <SidebarTrigger />
-    </>
+      <div className="flex m-auto">
+        <BookTicket />
+      </div>
+
+    </ >
   );
 };
 
